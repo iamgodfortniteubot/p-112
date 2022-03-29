83f103f1a6667e09ec44f338bb8d1fc1faf80e1f
iamgodfortniteubot <73544414+iamgodfortniteubot@users.noreply.github.com>
2022-03-29 15:31:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function  preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(640,480);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+tint_color = "";
+}
+
+function draw(){
+image(video,0,0,640,480);
+tint(tint_color);
+}
+
+function take_snapshot(){
+save('doremon.png');
+}
+
